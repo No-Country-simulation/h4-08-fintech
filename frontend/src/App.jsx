908809ec4b5f8aa99b01900609310e85f734bdce
agusmena <agusmena60@mail.com>
2025-01-14 12:36:25 -0300

    feature- maquetado pagina login e Inicio

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
-import './App.css'
+import "./App.css";
+import Inicio from "./pages/Inicio";
+import Login from "./pages/Login";
 
 function App() {
-
   return (
     <div>
-      <h1 className='bg-black text-cyan-50'>Home</h1>
+      <h1 className="bg-black text-cyan-50">Home</h1>
+      <Inicio />{" "}
+      <Login></Login>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

abf5e5f5cde4a72e243324d64ff83c11dac8efd7
agusmena <agusmena60@mail.com>
2025-01-15 16:17:45 -0300

    feature-onboarding bienvenido

--- frontend/src/pages/Onboarding/Onboarding.jsx
+++ frontend/src/pages/Onboarding/Onboarding.jsx
@@ -1,11 +1,11 @@
 import ArrowBack from '../../assets/icons/arrow-back.svg';
-import { EnableButton } from '../../components/common/EnableButton';
+import OnboardingStep1 from '../../components/Onboarding/OnboardingStep1';
 export const Onboarding = () => {
 
     return(
-        <div className="bg-gradient bg-center h-screen w-screen relative">
+        <div className="flex flex-col items-center justify-center bg-gradient bg-center h-screen w-screen relative">
             <button className='flex items-center justify-center w-[55px] h-[55px] bg-button border-2 border-gradient-stroke rounded-xl absolute top-5 left-5'><img src={ArrowBack} className='w-[24px]' alt="flecha hacia atras" /></button>
-            <EnableButton content='Continuar' available={true}/>
+         <OnboardingStep1/>
         </div>
     )
 }
--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import { Onboarding } from './pages/Onboarding/Onboarding';
+import OnboardingStep1 from './components/Onboarding/OnboardingStep1';
 
 export default function AppRouter() {
   return (
@@ -11,6 +12,7 @@ export default function AppRouter() {
         <Route path='/login' element={<Login/>} />
         <Route path='/registro' element={<Home/>} />
         <Route path='/onboarding' element={<Onboarding/>} />
+        <Route path='/onboarding1' element={<OnboardingStep1/>} />
       </Routes>
     </Router>
   );

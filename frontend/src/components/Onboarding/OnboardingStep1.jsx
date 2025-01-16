7d60031cdd51438875826cb72f611b1e52aaf90a
agusmena <agusmena60@mail.com>
2025-01-16 17:59:13 -0300

    empiezo flujo del onboarding

--- frontend/src/components/Onboarding/OnboardingStep1.jsx
+++ frontend/src/components/Onboarding/OnboardingStep1.jsx
@@ -1,7 +1,8 @@
-import logo from '../../assets/icons/logo/iUpi.svg'
-const OnboardingStep1 = () => {
-    return (
-        <div>
+/* eslint-disable react/prop-types */
+import logo from "../../assets/icons/logo/iUpi.svg";
+const OnboardingStep1 = ({ onNext }) => {
+  return (
+    <div>
       <div className="flex flex-col backdrop-blur-md mx-3 bg-white/50 rounded-lg shadow-lg p-8 max-w-sm  text-center">
         <h2 className="text-2xl font-bold text-black mb-4">Bienvenido a</h2>
         <div className="flex justify-center mt-3 mb-6">
@@ -15,9 +16,16 @@ const OnboardingStep1 = () => {
           experiencia financiera.
         </p>
       </div>
-
+      <div className="m-3">
+        <button
+          onClick={onNext}
+          className="flex w-full justify-center rounded-2xl bg-blue-500 px-3 py-2 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 "
+        >
+          Comenzar
+        </button>
+      </div>
     </div>
-    );
+  );
 };
 
 export default OnboardingStep1;
--- frontend/src/pages/Onboarding/Onboarding.jsx
+++ frontend/src/pages/Onboarding/Onboarding.jsx
@@ -1,11 +1,22 @@
-import ArrowBack from '../../assets/icons/arrow-back.svg';
-import OnboardingStep1 from '../../components/Onboarding/OnboardingStep1';
+import { useState } from "react";
+import ArrowBack from "../../assets/icons/arrow-back.svg";
+import OnboardingStep1 from "../../components/Onboarding/OnboardingStep1";
+import OnboardingCard from "../../components/Onboarding/OnboardingCard";
 export const Onboarding = () => {
+  const [step, SetStep] = useState(1);
 
-    return(
-        <div className="flex flex-col items-center justify-center bg-gradient bg-center h-screen w-screen relative">
-            <button className='flex items-center justify-center w-[55px] h-[55px] bg-button border-2 border-gradient-stroke rounded-xl absolute top-5 left-5'><img src={ArrowBack} className='w-[24px]' alt="flecha hacia atras" /></button>
-         <OnboardingStep1/>
-        </div>
-    )
-}
+  const handleNext = () => {
+    SetStep((prevStep) => prevStep + 1);
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center bg-gradient bg-center h-screen w-screen relative">
+      <button className="flex items-center justify-center w-[55px] h-[55px] bg-button border-2 border-gradient-stroke rounded-xl absolute top-5 left-5">
+        <img src={ArrowBack} className="w-[24px]" alt="flecha hacia atras" />
+      </button>
+      {step === 1 && <OnboardingStep1 onNext={handleNext} />}
+      {step === 2 && <OnboardingCard onNext={handleNext} />}
+    
+    </div>
+  );
+};

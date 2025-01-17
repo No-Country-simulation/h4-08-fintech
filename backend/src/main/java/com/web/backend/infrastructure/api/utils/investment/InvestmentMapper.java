f3b3663696383f3378246b7b13fe9d4a6cadf692
Chenhao <chenhaohu555@gmail.com>
2025-01-17 12:12:04 +0100

    feat: create default amount for investment

--- backend/src/main/java/com/web/backend/infrastructure/api/utils/investment/InvestmentMapper.java
+++ backend/src/main/java/com/web/backend/infrastructure/api/utils/investment/InvestmentMapper.java
@@ -6,13 +6,11 @@ import com.web.backend.application.dto.investment.InvestmentResponse;
 import com.web.backend.application.dto.news.NewsSourceRequest;
 import com.web.backend.domain.model.investment.Investment;
 import com.web.backend.domain.model.news.NewsSource;
-import org.mapstruct.BeanMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public interface InvestmentMapper {
+    @Mapping(target = "amount", defaultValue = "0.00f")
     Investment toInvestment(InvestmentRequest investmentRequest);
 
     InvestmentResponse toInvestmentResponse(Investment investment);

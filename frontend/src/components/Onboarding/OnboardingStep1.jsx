f1ab8f3aec63095ad0f1622344932a1c605b1457
agusmena <agusmena60@mail.com>
2025-01-17 09:55:23 -0300

    feature - agrego logica del flujo del dashboard

--- frontend/src/components/Onboarding/OnboardingStep1.jsx
+++ frontend/src/components/Onboarding/OnboardingStep1.jsx
@@ -1,6 +1,5 @@
-/* eslint-disable react/prop-types */
 import logo from "../../assets/icons/logo/iUpi.svg";
-const OnboardingStep1 = ({ onNext }) => {
+const OnboardingStep1 = () => {
   return (
     <div>
       <div className="flex flex-col backdrop-blur-md mx-3 bg-white/50 rounded-lg shadow-lg p-8 max-w-sm  text-center">
@@ -16,14 +15,7 @@ const OnboardingStep1 = ({ onNext }) => {
           experiencia financiera.
         </p>
       </div>
-      <div className="m-3">
-        <button
-          onClick={onNext}
-          className="flex w-full justify-center rounded-2xl bg-blue-500 px-3 py-2 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 "
-        >
-          Comenzar
-        </button>
-      </div>
+  
     </div>
   );
 };
--- frontend/src/components/common/EnableButton.jsx
+++ frontend/src/components/common/EnableButton.jsx
@@ -1,7 +1,13 @@
-export const EnableButton = ({ content, available}) => {
+/* eslint-disable react/prop-types */
+export const EnableButton = ({ content, available, onClick }) => {
     return (
-        <button className={`w-full h-12 my-2 flex items-center justify-center ${available? 'bg-blue-500 text-white' : 'bg-gray-50 text-gray-300'} font-semibold px-3 py-2 rounded-xl`} >
-            {content}
-        </button>
-    )
-}
+      <button
+        onClick={available ? onClick : null} // Sólo ejecuta onClick si el botón está habilitado
+        className={`w-full h-12 my-2 flex items-center justify-center ${
+          available ? 'bg-blue-500 text-white' : 'bg-gray-50 text-gray-300'
+        } font-semibold px-3 py-2 rounded-xl`}
+      >
+        {content}
+      </button>
+    );
+  };

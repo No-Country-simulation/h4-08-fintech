8cecf0d3aaace92c650c038b34481ea7cff6e377
Fernanda Sivila <fernandabsivila@gmail.com>
2025-01-17 14:54:18 -0300

    feat: Crea el componente reutilizable de onboarding y el step5 del mismo

--- frontend/src/components/common/EnableButton.jsx
+++ frontend/src/components/common/EnableButton.jsx
@@ -1,6 +1,6 @@
 export const EnableButton = ({ content, available}) => {
     return (
-        <button className={`w-full h-12 my-2 flex items-center justify-center ${available? 'bg-blue-500 text-white' : 'bg-gray-50 text-gray-300'} font-semibold px-3 py-2 rounded-xl`} >
+        <button className={`w-full h-12 my-2 flex items-center justify-center ${available? 'bg-blue-500 text-white' : 'bg-gray-50 text-gray-300'} font-semibold px-3 py-2 rounded-2xl`} >
             {content}
         </button>
     )
--- frontend/src/pages/Onboarding/Onboarding.jsx
+++ frontend/src/pages/Onboarding/Onboarding.jsx
@@ -1,11 +1,15 @@
 import ArrowBack from '../../assets/icons/arrow-back.svg';
 import { EnableButton } from '../../components/common/EnableButton';
+import { OnboardingStep5 } from '../../components/Onboarding/OnboardingStep5';
 export const Onboarding = () => {
 
     return(
-        <div className="bg-gradient bg-center h-screen w-screen relative">
-            <button className='flex items-center justify-center w-[55px] h-[55px] bg-button border-2 border-gradient-stroke rounded-xl absolute top-5 left-5'><img src={ArrowBack} className='w-[24px]' alt="flecha hacia atras" /></button>
-            <EnableButton content='Continuar' available={true}/>
+        <div className="bg-gradient bg-center h-screen w-screen relative pt-32 px-5 flex flex-col items-center font-jakarta">
+            <button className='flex items-center justify-center w-[55px] h-[55px] bg-blur border-2 border-gradient-stroke rounded-xl absolute top-5 left-3'><img src={ArrowBack} className='w-[24px]' alt="flecha hacia atras" /></button>
+            <OnboardingStep5 />
+            <div className='fixed bottom-1 w-full px-5'>
+                <EnableButton content='Continuar' available={true}/>
+            </div>
         </div>
     )
 }

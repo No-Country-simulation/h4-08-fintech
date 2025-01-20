8ff39f8dc32d1bee44ba585bb58ec359a57dffa2
Fernanda Sivila <fernandabsivila@gmail.com>
2025-01-20 10:24:38 -0300

    fix: arregla rutas de componentes en router.jsx

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,7 +1,8 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
-import Login from './pages/Login';
+import Login from './pages/Auth/Login';
 import { Onboarding } from './pages/Onboarding/Onboarding';
+import Register from './pages/Auth/Register';
 /* import OnboardingStep1 from './components/Onboarding/OnboardingStep1';
 import OnboardingStep2 from './components/Onboarding/OnboardingStep2';
 import OnboardingStep3 from "./components/Onboarding/OnboardingStep3" */
@@ -11,7 +12,7 @@ export default function AppRouter() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path='/login' element={<Login/>} />
-        <Route path='/registro' element={<Home/>} />
+        <Route path='/registro' element={<Register/>} />
         <Route path='/onboarding' element={<Onboarding/>} />
         {/* <Route path='/onboarding1' element={<OnboardingStep1/>} />
         <Route path='/onboarding2' element={<OnboardingStep2/>} />

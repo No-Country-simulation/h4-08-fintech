4f8f1b6aa61caf589a3d27c02ed418ffeeb2373c
Chenhao <chenhaohu555@gmail.com>
2025-01-22 22:07:12 +0100

    fix: add asset type to AssetResponse

--- backend/src/main/java/com/web/backend/application/DTO/asset/AssetResponse.java
+++ backend/src/main/java/com/web/backend/application/DTO/asset/AssetResponse.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 public record AssetResponse(
         String ticker,
         Float price,
+        AssetTypeResponse assetType,
         LocalDateTime updatedAt,
         LocalDateTime createdAt
 ) {

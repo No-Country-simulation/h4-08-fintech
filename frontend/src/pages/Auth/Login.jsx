bd6f4a354c542c3a0393b65cf6a2d372a654efab
nicolaselozano <nicolas.lozano@alumnos.frm.utn.edu.ar>
2025-01-23 00:53:09 -0300

    feat: autenticacion

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 
 import { ErrorMessage, Field, Form, Formik } from "formik";
 import * as yup from "yup";
+import { handleAuthGoogleLogin } from "../../services/api/auth/authGoogle";
 
 const Login = () => {
 
@@ -23,6 +24,10 @@ const Login = () => {
   const togglePassword = () => {
     setShowPassword(!showPassword);
   };
+
+  const handleGoogleRegister = () => {
+    handleAuthGoogleLogin();
+  }
 
   const handleSubmit = (data) => {
     console.log(data);
@@ -46,55 +51,55 @@ const Login = () => {
           initialValues={initialValues}
           validationSchema={validationSchema}
           onSubmit={handleSubmit}
-          >
-          {({errors, touched}) => (
-          <Form className="w-full">
-            <div className="mb-4">
-              <label className="block text-start text-sm font-medium text-gray-700">
-                Email
-              </label>
-              <Field
-                type="email"
-                name="email"
-                placeholder="email@email.com"
-                className={`mt-1 block w-full px-3 py-2 border ${errors.email && touched.email ? 'border-red-500' : 'border-gray-300'} rounded-xl shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
-              />
-              <ErrorMessage name="email" component="span" className="text-red-500 text-xs mt-1" />
-            </div>
-            <div className="relative mb-4">
-              <label className="block text-start text-sm font-medium text-gray-700">
-                Contraseña
-              </label>
-              <Field
-                placeholder="********"
-                type={showPassword ? "text" : "password"}
-                name="password"
-                className={`block mt-1 w-full px-3 py-2 border ${errors.password && touched.password ? 'border-red-500' : 'border-gray-300'} rounded-xl shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
-              />
-              <ErrorMessage name="password" component="span" className="text-start text-red-500 text-xs" />
+        >
+          {({ errors, touched }) => (
+            <Form className="w-full">
+              <div className="mb-4">
+                <label className="block text-start text-sm font-medium text-gray-700">
+                  Email
+                </label>
+                <Field
+                  type="email"
+                  name="email"
+                  placeholder="email@email.com"
+                  className={`mt-1 block w-full px-3 py-2 border ${errors.email && touched.email ? 'border-red-500' : 'border-gray-300'} rounded-xl shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
+                />
+                <ErrorMessage name="email" component="span" className="text-red-500 text-xs mt-1" />
+              </div>
+              <div className="relative mb-4">
+                <label className="block text-start text-sm font-medium text-gray-700">
+                  Contraseña
+                </label>
+                <Field
+                  placeholder="********"
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  className={`block mt-1 w-full px-3 py-2 border ${errors.password && touched.password ? 'border-red-500' : 'border-gray-300'} rounded-xl shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
+                />
+                <ErrorMessage name="password" component="span" className="text-start text-red-500 text-xs" />
 
-              <button
-                type="button"
-                onClick={togglePassword}
-                className="absolute inset-y-11 right-3 flex items-center"
-              >
-                {showPassword ? (
-                <FaRegEye className="text-lg" />
-              ) : (
-                <FaRegEyeSlash className="text-lg" />
-              )}
-              </button>
-            </div>
-            <div className="my-8">
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-2xl bg-blue-500 px-3 py-2 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 "
-              >
-                Iniciar Sesion
-              </button>
-            </div>
-          </Form>
-        )}
+                <button
+                  type="button"
+                  onClick={togglePassword}
+                  className="absolute inset-y-11 right-3 flex items-center"
+                >
+                  {showPassword ? (
+                    <FaRegEye className="text-lg" />
+                  ) : (
+                    <FaRegEyeSlash className="text-lg" />
+                  )}
+                </button>
+              </div>
+              <div className="my-8">
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-2xl bg-blue-500 px-3 py-2 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 "
+                >
+                  Iniciar Sesion
+                </button>
+              </div>
+            </Form>
+          )}
         </Formik>
         <div className="flex items-center justify-center my-4 w-full">
           <div className="flex-grow border-t border-gray-300"></div>
@@ -102,7 +107,8 @@ const Login = () => {
           <div className="flex-grow border-t border-gray-300"></div>
         </div>
         <button className="w-full my-2 flex items-center justify-center border border-blue-600 text-blue-600 font-semibold px-3 py-2 rounded-2xl hover:bg-blue-500 hover:text-white">
-          <FcGoogle className="text-2xl me-2" />
+          <FcGoogle className="text-2xl me-2"
+          onClick={handleGoogleRegister} />
           Continuar con Google
         </button>
         <button className="w-full my-2 flex items-center justify-center border border-blue-600 text-blue-600 font-semibold px-3 py-2 rounded-2xl hover:bg-blue-500 hover:text-white">
--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import Login from './pages/Auth/Login';
 import { Onboarding } from './pages/Onboarding/Onboarding';
 import Register from './pages/Auth/Register';
+import GoogleCallback from './pages/Auth/GoogleCallback';
 /* import OnboardingStep1 from './components/Onboarding/OnboardingStep1';
 import OnboardingStep2 from './components/Onboarding/OnboardingStep2';
 import OnboardingStep3 from "./components/Onboarding/OnboardingStep3" */
@@ -14,6 +15,7 @@ export default function AppRouter() {
         <Route path='/login' element={<Login/>} />
         <Route path='/registro' element={<Register/>} />
         <Route path='/onboarding' element={<Onboarding/>} />
+        <Route path='/oauth/success' element={<GoogleCallback/>} />
         {/* <Route path='/onboarding1' element={<OnboardingStep1/>} />
         <Route path='/onboarding2' element={<OnboardingStep2/>} />
         <Route path='/onboarding3' element={<OnboardingStep3/>} /> */}

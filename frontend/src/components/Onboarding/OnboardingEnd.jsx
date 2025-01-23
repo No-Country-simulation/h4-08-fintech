5b6f2a1c5df72adbc82b1a2c1a90433799d4c67c
agusmena <agusmena60@mail.com>
2025-01-23 18:23:21 -0300

    feature- agrego ultimo paso del onboarding

--- frontend/src/components/Onboarding/OnboardingEnd.jsx
+++ frontend/src/components/Onboarding/OnboardingEnd.jsx
@@ -1,9 +1,16 @@
 
 const OnboardingEnd = () => {
     return (
-        <div>
-            ONBOARDING END
-        </div>
+         <div data-aos="flip-left">
+      <div className="flex flex-col backdrop-blur-md bg-white/50 rounded-lg shadow-lg p-8 max-w-sm  text-center">
+      <h2 className="text-2xl font-bold text-black mb-4">¡Muchas gracias! </h2>
+               
+                <p className="text-gray-700 mb-4">
+                Gracias por compartir tu información. Ahora puedes explorar tu dashboard personalizado.                </p>
+             
+              </div>
+          
+            </div>
     );
 };
 

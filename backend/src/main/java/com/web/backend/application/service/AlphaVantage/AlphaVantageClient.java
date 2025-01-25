5a36c0cc3d58f67595486ec66df88a2aaccb7cfa
nicolaselozano <nicolas.lozano@alumnos.frm.utn.edu.ar>
2025-01-25 18:42:22 -0300

    feat: top_gainers query

--- backend/src/main/java/com/web/backend/application/service/AlphaVantage/AlphaVantageClient.java
+++ backend/src/main/java/com/web/backend/application/service/AlphaVantage/AlphaVantageClient.java
@@ -16,6 +16,17 @@ public class AlphaVantageClient {
     private final AppConfig appConfig;
     private final RestTemplate restTemplate = new RestTemplate();
 
+    public Map<String, Object> getTopGainers() {
+        String baseUrl = appConfig.getProperty("ALPHAVANTAGE_URL") + "/query";
+
+        String url = UriComponentsBuilder.fromUri(URI.create(baseUrl))
+                .queryParam("function", "TOP_GAINERS_LOSERS")
+                .queryParam("apikey", "demo")
+                .toUriString();
+
+        return restTemplate.getForObject(url, Map.class);
+    }
+
     public Map<String, Object> getStockData(String symbol) {
 
         String baseUrl = appConfig.getProperty("ALPHAVANTAGE_URL") + "/query";
@@ -30,7 +41,7 @@ public class AlphaVantageClient {
 
     public Map<String, Object> getOverviewData(String symbol) {
         String baseUrl = appConfig.getProperty("ALPHAVANTAGE_URL") + "/query";
-
+        System.out.println(symbol);
         String url = UriComponentsBuilder.fromUri(URI.create(baseUrl))
                 .queryParam("function", "OVERVIEW")
                 .queryParam("symbol", symbol)

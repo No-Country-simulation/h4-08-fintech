0168ce21eb8ef2edd863fa9ff647acd06421af38
nicolaselozano <nicolas.lozano@alumnos.frm.utn.edu.ar>
2025-01-26 17:57:48 -0300

    fix: retirando proxy

--- backend/src/main/java/com/web/backend/BackendApplication.java
+++ backend/src/main/java/com/web/backend/BackendApplication.java
@@ -5,22 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.util.Map;
-
 @SpringBootApplication
 @EnableFeignClients
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(BackendApplication.class);
-
-		// Establecer parámetros de configuración al inicio
-		app.setDefaultProperties(Map.of(
-				"server.address", "0.0.0.0", // Escuchar en todas las interfaces
-				"server.port", "8080"       // Establece el puerto (opcional)
-		));
-
-		app.run(args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
-}
 
+}

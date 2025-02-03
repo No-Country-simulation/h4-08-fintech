e32c7455eaa1eaf9d7cd8d112f8ea904c9f54239
MQuirico <128657405+MQuirico@users.noreply.github.com>
2025-02-03 16:20:36 -0300

    fix: issue regarding onboarding service fixed

--- frontend/src/services/api/onBoarding/onBoarding.jsx
+++ frontend/src/services/api/onBoarding/onBoarding.jsx
@@ -3,13 +3,23 @@ import {URL_API} from "../../../../vars"
 
 export default async function SendOnBoardingData(data) {
   try {
+    const forSending = {
+      estimatedExpenses: data.estimatedExpenses.payload,
+      estimatedIncome: data.estimatedIncome.payload,
+      financialGoalIds: data.financialGoalIds.payload,
+      financialKnowledge: data.financialKnowledge.payload,
+      fullName: data.fullName,
+      phoneNumber: data.phoneNumber,
+      riskTolerance: data.riskTolerance.payload,
+      savingsCapacity: data.savingsCapacity.payload
+    }
     const response = await fetch(`${URL_API}/customer?email=1`, {
       method: 'POST',
       credentials: true,
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(data),
+      body: JSON.stringify(forSending),
     });
 
     if (!response.ok) {
